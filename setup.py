601a8ec8241883b33a44281a79ba8e7206192619
Geoffrey Kasebacher <gkasenbacher@gmail.com>
2018-08-02 23:56:27 +0200

    ready for publishing

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cavemanstats",
-    version="1.0",
+    name="cavemanstatistics",
+    version="0.3",
     author="Geoffrey Kasenbacher",
     author_email="gkasenbacher@gmail.com",
-    description="Brute force, Exhaustive-Search for best R^2 in Linear Regression Models",
+    description="Exhaustive-Search for best R^2 in Linear Regression Models",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kgeoffrey/Caveman-Stats",
+    url="https://github.com/kgeoffrey/cavemanstats",
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'pandas', 'sklearn.linear_model', 'tabulate'],
+    install_requires=['numpy', 'pandas', 'scikit-learn', 'tabulate', 'scipy'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ),
 )
-
